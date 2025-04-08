ddad7a610fb1f37ef01ecedfa877f6ace14c502c
Robson  Alves <108527480+Robalves-5@users.noreply.github.com>
2025-04-07 21:16:07 -0300

    Update Automação delete.py

--- Automação delete.py
+++ Automação delete.py
@@ -1,11 +1,16 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do .env
+load_dotenv()
 
 # Dados de conexão
 dados_conexao = (
-    'DRIVER={SQL Server};' 
-    'SERVER=ROBSON-ALVES\\SQLEXPRESS;'
-    'DATABASE=vendas;'
-    'Trusted_Connection=yes;'
+    f"DRIVER={{SQL Server}};"
+    f"SERVER={os.getenv('DB_SERVER')};"
+    f"DATABASE={os.getenv('DB_NAME')};"
+    f"Trusted_Connection=yes;"
 )
 
 # Conectar ao banco de dados
@@ -16,25 +21,21 @@ print('Conexão bem-sucedida')
 cursor = conexao.cursor()
 
 # Verificar se a tabela 'produtos' existe
-if cursor.tables(table='produtos', tableType='TABLE').fetchone():
-    # Comando SQL para deletar uma linha (substitua condicao_deletar pela sua condição específica)
-    condicao_deletar = "ID_Produto = ?"
-    comando_delete = f"DELETE FROM produtos WHERE {condicao_deletar}"
-
-    # Valor para a condição (substitua pelo valor específico)
+if cursor.tables(table='produtos', tableType='TABLE', schema='dbo').fetchone():
+    # Verificar se o produto com o ID existe
     valor_condicao = 17
+    cursor.execute("SELECT * FROM produtos WHERE ID_Produto = ?", valor_condicao)
+    linha = cursor.fetchone()
 
-    # Executar o comando SQL de exclusão
-    cursor.execute(comando_delete, valor_condicao)
-
-    # Commit da transação
-    conexao.commit()
-
-    print('Exclusão bem-sucedida')
+    if linha:
+        cursor.execute("DELETE FROM produtos WHERE ID_Produto = ?", valor_condicao)
+        conexao.commit()
+        print(f"Produto com ID {valor_condicao} excluído com sucesso.")
+    else:
+        print(f"Nenhum produto com ID {valor_condicao} foi encontrado.")
 else:
-    print('A tabela "produtos" não foi encontrada no banco de dados "vendas".')
+    print('A tabela "produtos" não foi encontrada no banco de dados.')
 
 # Fechar o cursor e a conexão
 cursor.close()
 conexao.close()
-
