e7446534afb429da7b256a471707f6ffd011ed32
Robson  Alves <108527480+Robalves-5@users.noreply.github.com>
2025-04-07 21:18:54 -0300

    Update Automação insert.py

--- Automação insert.py
+++ Automação insert.py
@@ -1,11 +1,16 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente
+load_dotenv()
 
 # Dados de conexão
 dados_conexao = (
-    'DRIVER={SQL Server};' 
-    'SERVER=ROBSON-ALVES\\SQLEXPRESS;'  
-    'DATABASE=vendas;'
-    'Trusted_Connection=yes;'  
+    f"DRIVER={os.getenv('DB_DRIVER')};"
+    f"SERVER={os.getenv('DB_SERVER')};"
+    f"DATABASE={os.getenv('DB_DATABASE')};"
+    f"Trusted_Connection={os.getenv('DB_TRUSTED')};"
 )
 
 # Conectar ao banco de dados
@@ -17,28 +22,16 @@ cursor = conexao.cursor()
 
 # Verificar se a tabela 'produtos' existe
 if cursor.tables(table='produtos', tableType='TABLE').fetchone():
-    # Comando SQL com placeholders
     comando = """
         INSERT INTO produtos (ID_Produto, Nome_Produto, ID_Categoria, Marca_Produto, Num_Serie, Preco_Unit, Custo_Unit)
         VALUES (?, ?, ?, ?, ?, ?, ?)
     """
-
-    # Valores a serem inseridos
     valores = (16, 'Fone de Ouvido Tune T5001', 4, 'PHILCO', 'HDP-JB-091935', 900, 500)
-
-    # Executar o comando SQL com os valores
     cursor.execute(comando, valores)
-
-    # Commit da transação
     conexao.commit()
-
     print('Inserção bem-sucedida')
 else:
-    print('A tabela "produtos" não foi encontrada no banco de dados "vendas".')
+    print('A tabela "produtos" não foi encontrada no banco de dados.')
 
-# Fechar o cursor e a conexão
 cursor.close()
 conexao.close()
-
-
-
